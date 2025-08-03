e625eae711659f923201e4a9ab5d66af0566c683
Ishan <ishan.ghosh2004@gmail.com>
2025-08-03 13:56:17 +0530

    Update Reverse_Number.java

--- Reverse_Number.java
+++ Reverse_Number.java
@@ -1,29 +1,30 @@
 class Solution {
     public int reverse(int x) {
         int y = x;
+        int r = 0;
         int si = 0;
         if (x < 0) {
             x = x * -1;
-            si++;
-        } 
-        int r = 0;
+            si = 1;
+        }
         while (x > 0) {
             int n = x % 10;
-            r = (r * 10) + n;
+            if (r > (Integer.MAX_VALUE - n) / 10) {
+                return 0;
+            }
+            r = r * 10 + n;
             x /= 10;
         }
         if (y < 0) {
-            r = r * -1;
+            r = -r;
         }
-        double a = Math.pow(-2, 31);
-        double b = Math.pow(2, 31);
-        double c = Math.pow(2, 32);
-        if (si > 0) {
-            //represents signed integer
-            if (r >= a && r <= (b - 1)) {
-                return 0;
-            }
-        } 
+        double a = Math.pow(-2, 31); // -2147483648
+        double b = Math.pow(2, 31); // 2147483648
+
+        if (r < a || r >= b) {
+            return 0;
+        }
+
         return r;
     }
 }

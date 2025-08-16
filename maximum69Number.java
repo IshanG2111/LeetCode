876107ec41ffd0f16903fb06321a848ed4480b34
Ishan <ishan.ghosh2004@gmail.com>
2025-08-16 16:33:05 +0530

    Create maximum69Number.java

---
+++ maximum69Number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximum69Number (int num) {
+        String str=Integer.toString(num);
+        char[] digits = str.toCharArray(); 
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == '6') {
+                digits[i] = '9'; 
+                break; 
+            }
+        }
+        String result = new String(digits); 
+        return Integer.parseInt(result); 
+    }
+}

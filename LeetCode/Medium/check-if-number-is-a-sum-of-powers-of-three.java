adcc16b20b02350e4a975a3e74fd463ddc1cdd71
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 02:08:24 +0530

    Added sync for Check if Number is a Sum of Powers of Three

--- LeetCode/Medium/check-if-number-is-a-sum-of-powers-of-three.java
+++ LeetCode/Medium/check-if-number-is-a-sum-of-powers-of-three.java
@@ -3,34 +3,14 @@
 // Difficulty: Medium
 // Language: java
 
-// class Solution {
-//     public boolean checkPowersOfThree(int n) {
-//         while(n>1){
-//             if(n%3==2){
-//                 return false;
-//             }
-//             n/=3;
-//         }
-//         return true;
-//     }
-// }
 class Solution {
     public boolean checkPowersOfThree(int n) {
-        int original=n;
-        int count=0;
-        int r=0;
-        int sum=0;
-        while(n>0){
-            r=n%3;
-            if(r>1){
+        while(n>1){
+            if(n%3==2){
                 return false;
             }
-            if(r==1){
-                sum+=(int)Math.pow(3,count);
-            }
             n/=3;
-            count++;
         }
-        return sum==original;
+        return true;
     }
 }

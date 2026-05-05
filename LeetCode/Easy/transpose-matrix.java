5fc24047b79c9fb95a80d89d86038d72cbe1a0db
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 02:10:02 +0530

    Added sync for Transpose Matrix

---
+++ LeetCode/Easy/transpose-matrix.java
@@ -0,0 +1,18 @@
+// Title: Transpose Matrix
+// URL: https://leetcode.com/problems/transpose-matrix/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int r=matrix.length;
+        int c=matrix[0].length;
+        int [][]tr=new int[c][r];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                tr[j][i]=matrix[i][j];
+            }
+        }
+        return tr;
+    }
+}

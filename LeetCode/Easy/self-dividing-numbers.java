5f5744b5058521885576a06dbca8edd09a01fe5f
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:50:39 +0530

    Added sync for Self Dividing Numbers

--- LeetCode/Easy/self-dividing-numbers.java
+++ LeetCode/Easy/self-dividing-numbers.java
@@ -3,4 +3,26 @@
 // Difficulty: Easy
 // Language: java
 
-// Add your solution here!
+class Solution {
+    private boolean isTrue(int num) {
+        int ori=num;
+        int d=0;
+        while(num>0){
+            d=num%10;
+            if(d==0 || ori%d!=0){
+                return false;
+            }
+            num/=10;
+        }
+        return true;
+    }
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> result = new ArrayList<>();
+        for(int i=left;i<=right;i++){
+            if(isTrue(i)){
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}

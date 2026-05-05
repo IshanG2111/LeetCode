81402de95e35bafa4681d002f450c57061394d9e
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:50:17 +0530

    Added sync for Rotate List

--- LeetCode/Medium/rotate-list.java
+++ LeetCode/Medium/rotate-list.java
@@ -3,4 +3,37 @@
 // Difficulty: Medium
 // Language: java
 
-// Add your solution here!
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode curr = head;
+        int len = 1;
+        while (curr.next != null) {
+            curr = curr.next;
+            len++;
+        }
+        ListNode tail = curr;
+        k = k % len;
+        if (k == 0)
+            return head;
+        tail.next = head;
+        curr = head;
+        for (int i = 0; i < len - k - 1; i++)
+            curr = curr.next;
+        ListNode newHead = curr.next;
+        curr.next = null;
+        return newHead;
+    }
+}

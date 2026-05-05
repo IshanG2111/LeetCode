72cce3972dcb3f28c4bf22df4d03c8c2cab6ac6f
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:50:47 +0530

    Added sync for Rotate Function

--- LeetCode/Medium/rotate-function.java
+++ LeetCode/Medium/rotate-function.java
@@ -3,4 +3,20 @@
 // Difficulty: Medium
 // Language: java
 
-// Add your solution here!
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        int f=0;   
+        for (int i=0;i<n;i++){
+            sum+=nums[i];
+            f+=i*nums[i];
+        }
+        int res=f;
+        for (int i=n-1;i>=1;i--){
+            f=f+sum-n*nums[i];
+            res=Math.max(res, f);
+        }
+        return res;
+    }
+}

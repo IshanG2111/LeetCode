ee363109b8936109ce2f6db44b67ed9c5549a5ee
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:50:35 +0530

    Added sync for Rotated Digits

--- LeetCode/Medium/rotated-digits.java
+++ LeetCode/Medium/rotated-digits.java
@@ -3,4 +3,41 @@
 // Difficulty: Medium
 // Language: java
 
-// Add your solution here!
+class Solution {
+    public int rotatedDigits(int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 0);
+        map.put(1, 1);
+        map.put(2, 5);
+        map.put(5, 2);  
+        map.put(2, 5);
+        map.put(6, 9);
+        map.put(8, 8);
+        map.put(9, 6);
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            int num = i;
+            int original = num;
+            int rotated = 0;
+            int pos = 1;
+            boolean isGood = false;
+            while (num > 0) {
+                int digit = num % 10;
+                if (!map.containsKey(digit)) {
+                    isGood = false;
+                    break;
+                }
+                if (digit == 6 || digit == 9|| digit==2 || digit==5) {
+                    isGood = true;
+                }
+                rotated = map.get(digit) * pos + rotated;
+                pos *= 10;
+                num /= 10;
+            }
+            if (isGood && rotated != original) {
+                count++;
+            }
+        }
+        return count;
+    }
+}

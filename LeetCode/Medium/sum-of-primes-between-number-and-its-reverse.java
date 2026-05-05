9e3b644d2eacc436e69e6316734ea755be0325ff
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:50:30 +0530

    Added sync for Sum of Primes Between Number and Its Reverse

--- LeetCode/Medium/sum-of-primes-between-number-and-its-reverse.java
+++ LeetCode/Medium/sum-of-primes-between-number-and-its-reverse.java
@@ -3,4 +3,31 @@
 // Difficulty: Medium
 // Language: java
 
-// Add your solution here!
+class Solution {
+    public int sumOfPrimesInRange(int n) {
+        int r=0;
+        int temp=n;
+        while(temp>0){
+            r=(r*10)+(temp%10);
+            temp/=10;
+        }
+        int l=Math.min(n,r);
+        int h=Math.max(n,r);
+        int ans=0;
+        for(int i=l;i<=h;i++){
+            if(i<2){
+                continue;
+            }
+            boolean ok=true;
+            for(int j=2;j*j<=i;j++){
+                if(i%j==0){
+                    ok=false;
+                }
+            }
+            if(ok==true){
+                ans+=i;
+            }
+        }
+            return ans;
+    }
+}

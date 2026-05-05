6add35f4211b44316d2c310dad1eb34dda6e2da9
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 02:08:26 +0530

    Added sync for Power of Three

--- LeetCode/Easy/power-of-three.java
+++ LeetCode/Easy/power-of-three.java
@@ -5,14 +5,14 @@
 
 class Solution {
     public boolean isPowerOfThree(int n) {
-        int original = n;
+int original=n;
         if (n < 0) {
             return false;
         }
-        double base = 3;
-        double count = Math.log(n) / Math.log(base);
+        double base=3;
+        double count= Math.log(n) / Math.log(base);
         int m = (int) Math.round(count);
-        if (Math.pow(base, m) == original) {
+        if(Math.pow(base,m)==original){
             return true;
         }
         return false;

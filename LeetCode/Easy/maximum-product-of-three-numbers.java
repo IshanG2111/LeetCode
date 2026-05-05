bd647050e3ed018ffd992d2b0908cac892ec9177
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 02:08:55 +0530

    Added sync for Maximum Product of Three Numbers

---
+++ LeetCode/Easy/maximum-product-of-three-numbers.java
@@ -0,0 +1,15 @@
+// Title: Maximum Product of Three Numbers
+// URL: https://leetcode.com/problems/maximum-product-of-three-numbers/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int product1 = nums[n - 1] * nums[n - 2] * nums[n - 3]; 
+        int product2 = nums[0] * nums[1] * nums[n - 1];
+
+        return Math.max(product1, product2);
+    }
+}

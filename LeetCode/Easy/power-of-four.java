bf680c9ef8b69cc10f7074bf146a31295759d6be
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 02:08:29 +0530

    Added sync for Power of Four

---
+++ LeetCode/Easy/power-of-four.java
@@ -0,0 +1,20 @@
+// Title: Power of Four
+// URL: https://leetcode.com/problems/power-of-four/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        int original=n;
+        if (n < 0) {
+            return false;
+        }
+        double base=4;
+        double count= Math.log(n) / Math.log(base);
+        int m = (int) Math.round(count);
+        if(Math.pow(base,m)==original){
+            return true;
+        }
+        return false;
+    }
+}

cc31c615860287a1e459b19dc4a371bd9ac59918
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 02:05:56 +0530

    Added sync for Count Partitions with Even Sum Difference

---
+++ LeetCode/Easy/count-partitions-with-even-sum-difference.java
@@ -0,0 +1,17 @@
+// Title: Count Partitions with Even Sum Difference
+// URL: https://leetcode.com/problems/count-partitions-with-even-sum-difference/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public int countPartitions(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        if(sum%2==0){
+            return nums.length-1;
+        }
+        return 0;
+    }
+}

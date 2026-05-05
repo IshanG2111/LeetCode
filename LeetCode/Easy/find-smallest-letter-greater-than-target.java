2af265d9a5dcd84d6afc61b316a23fcb59d97445
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:53:32 +0530

    Added sync for Find Smallest Letter Greater Than Target

---
+++ LeetCode/Easy/find-smallest-letter-greater-than-target.java
@@ -0,0 +1,18 @@
+// Title: Find Smallest Letter Greater Than Target
+// URL: https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int t=(int)target;
+        int mint=0;
+        for(int i=0;i<letters.length;i++){
+            mint=(int)letters[i]-t;
+            if(mint>0) {
+                return letters[i];
+            }
+        }
+        return letters[0];
+    }
+}

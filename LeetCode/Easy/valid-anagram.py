2b9e799157fa9c661bbd600f7f84ecc27cff2749
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 02:01:16 +0530

    Added sync for Valid Anagram

--- LeetCode/Easy/valid-anagram.py
+++ LeetCode/Easy/valid-anagram.py
@@ -7,12 +7,18 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if(len(s)!=len(t)):
             return False
-        countS={}
-        countT={}
-        for i in range(len(s)):
-            countS[s[i]]=1+countS.get(s[i],0)
-            countT[t[i]]=1+countT.get(t[i],0)
-        for c in countS:
-            if countS[c]!=countT.get(c,0):
-                return False
-        return True
+        sorted_s=sorted(s)
+        sorted_t=sorted(t)
+        if sorted_s==sorted_t:
+            return True
+        return False
+
+        # countS={}
+        # countT={}
+        # for i in range(len(s)):
+        #     countS[s[i]]=1+countS.get(s[i],0)
+        #     countT[t[i]]=1+countT.get(t[i],0)
+        # for c in countS:
+        #     if countS[c]!=countT.get(c,0):
+        #         return False
+        # return True

afdfa29cb750dd5ddc0e5a85fdadf443b74eeb4c
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:19:43 +0530

    Added sync for Count the Digits That Divide a Number

---
+++ LeetCode/Easy/count-the-digits-that-divide-a-number.java
@@ -0,0 +1,6 @@
+// Title: Count the Digits That Divide a Number
+// URL: https://leetcode.com/problems/count-the-digits-that-divide-a-number/
+// Difficulty: Easy
+// Language: java
+
+// Add your solution here!

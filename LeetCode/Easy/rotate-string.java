8383c14cafc85815c358961afc714cf645f4688f
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:50:28 +0530

    Added sync for Rotate String

--- LeetCode/Easy/rotate-string.java
+++ LeetCode/Easy/rotate-string.java
@@ -3,4 +3,12 @@
 // Difficulty: Easy
 // Language: java
 
-// Add your solution here!
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        String combined = s + s;
+        return combined.contains(goal);
+    }
+}

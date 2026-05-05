be7f675d3036d7dd7031955e93f356ce097aac7e
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:50:33 +0530

    Added sync for Count Indices With Opposite Parity

--- LeetCode/Easy/count-indices-with-opposite-parity.java
+++ LeetCode/Easy/count-indices-with-opposite-parity.java
@@ -3,4 +3,26 @@
 // Difficulty: Easy
 // Language: java
 
-// Add your solution here!
+class Solution {
+    public int[] countOppositeParity(int[] nums) {
+        int n=nums.length;
+        int[] res=new int[n];
+        int even=0;
+        int odd=0;
+        for(int i=n-1;i>=0;i--){
+            if((nums[i]&1)==0){
+                res[i]=odd;
+            }
+            else{
+                res[i]=even;
+            }
+            if((nums[i]&1)==0){
+                even++;
+            }
+            else{
+                odd++;
+            }
+        }
+        return res;
+    }
+}

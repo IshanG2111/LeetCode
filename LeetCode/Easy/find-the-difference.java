513f691c7ca7082d3a1cb4146dbd044226fd4ccf
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 02:09:36 +0530

    Added sync for Find the Difference

---
+++ LeetCode/Easy/find-the-difference.java
@@ -0,0 +1,20 @@
+// Title: Find the Difference
+// URL: https://leetcode.com/problems/find-the-difference/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int i;
+        int S=0;
+        int T=0;
+        for(i =0;i<s.length();i++){
+            S+=s.charAt(i);
+        }
+        for(i=0;i<t.length();i++){
+            T+=t.charAt(i);
+        }
+        char result=(char)(T-S);
+        return result;
+    }
+}

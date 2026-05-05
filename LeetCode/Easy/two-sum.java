ab7425f8ca4c1b3656f66b3b97efdc60631a291a
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:52:26 +0530

    Added sync for Two Sum

---
+++ LeetCode/Easy/two-sum.java
@@ -0,0 +1,20 @@
+// Title: Two Sum
+// URL: https://leetcode.com/problems/two-sum/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int i, j;
+        int[] result = {};
+        for (i = 0; i < nums.length; i++) {
+            j = target - nums[i];
+            if (map.containsKey(j)) {
+                return new int[] { map.get(j), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}

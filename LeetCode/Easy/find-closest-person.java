ecb98e0a5e93b4fdfcc8286f3cbbca41c62b882b
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 02:07:47 +0530

    Added sync for Find Closest Person

---
+++ LeetCode/Easy/find-closest-person.java
@@ -0,0 +1,18 @@
+// Title: Find Closest Person
+// URL: https://leetcode.com/problems/find-closest-person/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int diff1 = Math.abs(x - z);
+        int diff2 = Math.abs(y - z);
+        if(diff1<diff2){
+            return 1;
+        }
+        else if(diff2<diff1){
+            return 2;
+        }
+        return 0;
+    }
+}

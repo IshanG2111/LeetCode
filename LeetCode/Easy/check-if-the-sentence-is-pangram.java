2e694e7fbf3a09a0811dbf9b74781d85f7e381c3
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:19:47 +0530

    Added sync for Check if the Sentence Is Pangram

---
+++ LeetCode/Easy/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,6 @@
+// Title: Check if the Sentence Is Pangram
+// URL: https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+// Difficulty: Easy
+// Language: java
+
+// Add your solution here!

dcdd97d2ecd13892b37c6d54e7ff8bed79dfc75c
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:56:15 +0530

    Added sync for Guess Number Higher or Lower

---
+++ LeetCode/Easy/guess-number-higher-or-lower.py
@@ -0,0 +1,26 @@
+// Title: Guess Number Higher or Lower
+// URL: https://leetcode.com/problems/guess-number-higher-or-lower/
+// Difficulty: Easy
+// Language: python3
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        left=1
+        right=n
+        while True:
+            mid=(left+right)//2
+            result=guess(mid)
+            if (result>0):
+                left=mid+1
+            elif(result<0):
+                right=mid-1
+            else:
+                return mid  
+        

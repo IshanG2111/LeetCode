2ede06c77f0718ad4b4381ad5a7eec726f38215b
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:50:59 +0530

    Added sync for Minimum Number Game

--- LeetCode/Easy/minimum-number-game.java
+++ LeetCode/Easy/minimum-number-game.java
@@ -3,4 +3,15 @@
 // Difficulty: Easy
 // Language: java
 
-// Add your solution here!
+class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            int temp=nums[i];
+            nums[i]=nums[i+1];
+            nums[i+1]=temp;
+            i++;
+        }
+        return nums;
+    }
+}

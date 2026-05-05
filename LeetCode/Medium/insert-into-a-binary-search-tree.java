8c64c92e90d140b6af9b81a38c0f6ca6634a3584
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:19:49 +0530

    Added sync for Insert into a Binary Search Tree

---
+++ LeetCode/Medium/insert-into-a-binary-search-tree.java
@@ -0,0 +1,6 @@
+// Title: Insert into a Binary Search Tree
+// URL: https://leetcode.com/problems/insert-into-a-binary-search-tree/
+// Difficulty: Medium
+// Language: java
+
+// Add your solution here!

5ef39258997bedb5764ee20b30430b07adeb6e12
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:19:35 +0530

    Added sync for Count Indices With Opposite Parity

---
+++ LeetCode/Easy/count-indices-with-opposite-parity.java
@@ -0,0 +1,6 @@
+// Title: Count Indices With Opposite Parity
+// URL: https://leetcode.com/problems/count-indices-with-opposite-parity/
+// Difficulty: Easy
+// Language: java
+
+// Add your solution here!

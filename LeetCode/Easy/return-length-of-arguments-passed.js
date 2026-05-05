9ce6365ca18d24abde1fdb587c5690ee81bc393f
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:56:13 +0530

    Added sync for Return Length of Arguments Passed

---
+++ LeetCode/Easy/return-length-of-arguments-passed.js
@@ -0,0 +1,16 @@
+// Title: Return Length of Arguments Passed
+// URL: https://leetcode.com/problems/return-length-of-arguments-passed/
+// Difficulty: Easy
+// Language: javascript
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */

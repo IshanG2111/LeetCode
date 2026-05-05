68cd023b0ca0dd9af229542683b20971605046a4
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 02:05:25 +0530

    Added sync for Palindrome Number

---
+++ LeetCode/Easy/palindrome-number.java
@@ -0,0 +1,26 @@
+// Title: Palindrome Number
+// URL: https://leetcode.com/problems/palindrome-number/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        int original=x;
+        if(x<0){
+            return false;
+        }
+        int rem;
+        int rev=0;
+        while(x!=0){
+            rem=(int)(x%10);
+            rev=(rev*10)+rem;
+            x=x/10;
+        }
+        if(rev==original){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}

bd511abb4f0818445f89af57b863f630c5029cda
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 02:06:41 +0530

    Added sync for Binary Search

---
+++ LeetCode/Easy/binary-search.java
@@ -0,0 +1,15 @@
+// Title: Binary Search
+// URL: https://leetcode.com/problems/binary-search/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public int search(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}

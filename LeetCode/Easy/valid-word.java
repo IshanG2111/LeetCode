3b6f6ddbd60c399b574ad3702207eab01fb163f6
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 02:10:05 +0530

    Added sync for Valid Word

---
+++ LeetCode/Easy/valid-word.java
@@ -0,0 +1,32 @@
+// Title: Valid Word
+// URL: https://leetcode.com/problems/valid-word/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3) {
+            return false;
+        }
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+
+        for (char c : word.toCharArray()) {
+            if (!Character.isLetterOrDigit(c)) {
+                return false;
+            }
+
+            if (Character.isLetter(c)) {
+                char lower = Character.toLowerCase(c);
+                if ("aeiou".indexOf(lower) >= 0) {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+}

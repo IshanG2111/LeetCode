36f0644ea6f17de3cf640f017e9c0265bf424b01
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:56:08 +0530

    Added sync for Counter II

---
+++ LeetCode/Easy/counter-ii.js
@@ -0,0 +1,34 @@
+// Title: Counter II
+// URL: https://leetcode.com/problems/counter-ii/
+// Difficulty: Easy
+// Language: javascript
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count=init;
+    function increment(){
+        return ++count;
+    }
+    function decrement(){
+        return --count;
+    }
+    function reset(){
+        count=init;
+        return count;
+    }
+    return {
+        increment: increment,
+        decrement: decrement,
+        reset: reset
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */

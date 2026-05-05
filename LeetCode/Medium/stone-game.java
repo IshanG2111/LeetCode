10ea184c7959775a4c903d4d96aad51f696f2f2e
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:52:09 +0530

    Added sync for Stone Game

---
+++ LeetCode/Medium/stone-game.java
@@ -0,0 +1,10 @@
+// Title: Stone Game
+// URL: https://leetcode.com/problems/stone-game/
+// Difficulty: Medium
+// Language: java
+
+class Solution {
+    public boolean stoneGame(int[] piles) {
+       return true; 
+    }
+}

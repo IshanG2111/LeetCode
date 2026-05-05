de3b6df429d2c1b4c3359d340390e5fb71b06f12
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:52:14 +0530

    Added sync for Matrix Similarity After Cyclic Shifts

---
+++ LeetCode/Easy/matrix-similarity-after-cyclic-shifts.java
@@ -0,0 +1,23 @@
+// Title: Matrix Similarity After Cyclic Shifts
+// URL: https://leetcode.com/problems/matrix-similarity-after-cyclic-shifts/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public boolean areSimilar(int[][] mat, int k) {
+        int m = mat.length;
+        int n = mat[0].length;
+        k %= n;
+        if (k == 0){
+            return true;
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) { 
+                if (mat[i][j] != mat[i][(j + k) % n]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}

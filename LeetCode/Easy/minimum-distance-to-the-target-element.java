ccae5ec82b18a8e7bea6748faf6b457798a31706
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:51:37 +0530

    Added sync for Minimum Distance to the Target Element

---
+++ LeetCode/Easy/minimum-distance-to-the-target-element.java
@@ -0,0 +1,24 @@
+// Title: Minimum Distance to the Target Element
+// URL: https://leetcode.com/problems/minimum-distance-to-the-target-element/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int rr = nums.length;
+        int lr = nums.length;
+        for (int i = start; i >= 0; i--) {
+            if (nums[i] == target) {
+                lr = start - i;
+                break;
+            }
+        }
+        for (int i = start; i < nums.length; i++) {
+            if (nums[i] == target) {
+                rr = i - start;
+                break;
+            }
+        }
+        return Math.abs(Math.min(rr, lr));
+    }
+}

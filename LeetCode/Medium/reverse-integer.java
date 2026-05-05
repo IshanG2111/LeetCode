77c50fa139b6ca53d65c14aece6b58a8f422f67d
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:55:22 +0530

    Added sync for Reverse Integer

--- LeetCode/Medium/reverse-integer.java
+++ LeetCode/Medium/reverse-integer.java
@@ -5,18 +5,30 @@
 
 class Solution {
     public int reverse(int x) {
-        int rev=0;
-        while(x!=0){
-            int pop=x%10;
-            x/=10;
-            if(rev>Integer.MAX_VALUE/10 || (rev==Integer.MAX_VALUE/10 && pop>7)){
-                return 0;
-            }
-            if(rev<Integer.MIN_VALUE/10 || (rev==Integer.MIN_VALUE/10 && pop< -8)){
-                return 0;
-            }
-            rev=rev*10+pop;
+        int y = x;
+        int r = 0;
+        int si = 0;
+        if (x < 0) {
+            x = x * -1;
+            si = 1;
         }
-        return rev;
+        while (x != 0) {
+            int n = x % 10;
+            if (r > (Integer.MAX_VALUE - n) / 10) {
+                return 0;
+            }
+            r = r * 10 + n;
+            x /= 10;
+        }
+        if (y < 0) {
+            r = -r;
+        }
+        double a = Math.pow(-2, 31); // -2147483648
+        double b = Math.pow(2, 31); // 2147483648
+
+        if (r < a || r >= b) {
+            return 0;
+        }
+        return r;
     }
 }

751c781d0cdcf549dfb1cbd9aeaf516b2bcf5613
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:54:43 +0530

    Added sync for Find Peak Element

---
+++ LeetCode/Medium/find-peak-element.java
@@ -0,0 +1,21 @@
+// Title: Find Peak Element
+// URL: https://leetcode.com/problems/find-peak-element/
+// Difficulty: Medium
+// Language: java
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int low=0;
+        int high=nums.length-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if (nums[mid]<nums[mid+1]){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        return low;
+    }
+}

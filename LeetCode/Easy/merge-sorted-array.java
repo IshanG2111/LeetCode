a3b6efcf9f0bf38cf7b24f69d6d1911eb5c74af8
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 02:08:52 +0530

    Added sync for Merge Sorted Array

---
+++ LeetCode/Easy/merge-sorted-array.java
@@ -0,0 +1,21 @@
+// Title: Merge Sorted Array
+// URL: https://leetcode.com/problems/merge-sorted-array/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int l1=nums1.length;
+        int l2=nums2.length;
+        int count=0;
+        
+            for(int i=m;i< l1;i++){
+                nums1[i]=nums2[count];
+                count++;
+            }
+        Arrays.sort(nums1);
+        for(int i=0;i<l1;i++){
+            System.out.print(nums1[i]+",");
+        }
+    }
+}

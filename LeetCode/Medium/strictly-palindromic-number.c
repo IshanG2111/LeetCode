8d5f4185b9add598e95bffa7e6ce6cc401a0ff73
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 02:07:50 +0530

    Added sync for Strictly Palindromic Number

---
+++ LeetCode/Medium/strictly-palindromic-number.c
@@ -0,0 +1,8 @@
+// Title: Strictly Palindromic Number
+// URL: https://leetcode.com/problems/strictly-palindromic-number/
+// Difficulty: Medium
+// Language: c
+
+bool isStrictlyPalindromic(int n) {
+    return false;
+}

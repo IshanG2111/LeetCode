f0cef74bf172bc506f7f48d74c8bc0d640016e43
Ishan <ishan.ghosh2004@gmail.com>
2026-05-06 01:50:49 +0530

    Added sync for Check if the Sentence Is Pangram

--- LeetCode/Easy/check-if-the-sentence-is-pangram.java
+++ LeetCode/Easy/check-if-the-sentence-is-pangram.java
@@ -3,4 +3,20 @@
 // Difficulty: Easy
 // Language: java
 
-// Add your solution here!
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        if(sentence.length()< 26){
+            return false;
+        }
+        boolean[] seen = new boolean[26];
+        for (char c : sentence.toCharArray()) {
+            seen[c - 'a'] = true;
+        }
+        for (boolean letter : seen) {
+            if (!letter) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

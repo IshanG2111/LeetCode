d82b695fa1b6a9d03c798909f80acca7b32be318
Ishan <ishan.ghosh2004@gmail.com>
2026-05-14 12:27:11 +0530

    Added sync for Separate the Digits in an Array

---
+++ LeetCode/Easy/separate-the-digits-in-an-array.java
@@ -0,0 +1,6 @@
+// Title: Separate the Digits in an Array
+// URL: https://leetcode.com/problems/separate-the-digits-in-an-array/
+// Difficulty: Easy
+// Language: java
+
+// Add your solution here!

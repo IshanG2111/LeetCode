35bb26fb864a4601512b7a5433bf91dde56d756a
Ishan <ishan.ghosh2004@gmail.com>
2026-05-14 12:27:22 +0530

    Added sync for Maximum Number of Words Found in Sentences

---
+++ LeetCode/Easy/maximum-number-of-words-found-in-sentences.java
@@ -0,0 +1,6 @@
+// Title: Maximum Number of Words Found in Sentences
+// URL: https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+// Difficulty: Easy
+// Language: java
+
+// Add your solution here!

21056fb3267b9148d4eab751e1eb66f5e6dd760d
Ishan <ishan.ghosh2004@gmail.com>
2026-05-14 12:27:13 +0530

    Added sync for Minimize Array Sum Using Divisible Replacements

---
+++ LeetCode/Medium/minimize-array-sum-using-divisible-replacements.java
@@ -0,0 +1,6 @@
+// Title: Minimize Array Sum Using Divisible Replacements
+// URL: https://leetcode.com/problems/minimize-array-sum-using-divisible-replacements/
+// Difficulty: Medium
+// Language: java
+
+// Add your solution here!

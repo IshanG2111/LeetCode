df2a99d6f7a04b15a4439c936dcdd7ebd0035610
Ishan <ishan.ghosh2004@gmail.com>
2026-05-14 12:27:07 +0530

    Added sync for Find the Maximum Achievable Number

---
+++ LeetCode/Easy/find-the-maximum-achievable-number.java
@@ -0,0 +1,6 @@
+// Title: Find the Maximum Achievable Number
+// URL: https://leetcode.com/problems/find-the-maximum-achievable-number/
+// Difficulty: Easy
+// Language: java
+
+// Add your solution here!

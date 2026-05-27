54cbc9b80bf4e7759adde71e82b0690045d66caf
Ishan <ishan.ghosh2004@gmail.com>
2026-05-28 02:37:02 +0530

    Added sync for Longest Substring Without Repeating Characters

---
+++ LeetCode/Medium/longest-substring-without-repeating-characters.java
@@ -0,0 +1,6 @@
+// Title: Longest Substring Without Repeating Characters
+// URL: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+// Difficulty: Medium
+// Language: java
+
+// Add your solution here!

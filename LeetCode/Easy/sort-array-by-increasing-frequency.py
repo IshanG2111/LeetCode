22cd96f92142d5ca5cdd83f56a05a6cd1be04116
Ishan <ishan.ghosh2004@gmail.com>
2026-05-28 02:36:53 +0530

    Added sync for Sort Array by Increasing Frequency

---
+++ LeetCode/Easy/sort-array-by-increasing-frequency.py
@@ -0,0 +1,6 @@
+// Title: Sort Array by Increasing Frequency
+// URL: https://leetcode.com/problems/sort-array-by-increasing-frequency/
+// Difficulty: Easy
+// Language: python3
+
+// Add your solution here!

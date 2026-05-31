2ebcaadb9276ee76289dc2a1ef3469d81c14e75f
Ishan <ishan.ghosh2004@gmail.com>
2026-06-01 00:13:39 +0530

    Added sync for Minimum Common Value

---
+++ LeetCode/Easy/minimum-common-value.java
@@ -0,0 +1,22 @@
+// Title: Minimum Common Value
+// URL: https://leetcode.com/problems/minimum-common-value/
+// Difficulty: Easy
+// Language: java
+
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            }
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return -1;
+    }
+}

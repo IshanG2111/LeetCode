0838ed48bdaeb36ba3a1de8d400d0184d7f39533
Ishan <ishan.ghosh2004@gmail.com>
2026-06-21 00:11:43 +0530

    Added sync for Find the Highest Altitude

--- LeetCode/Easy/find-the-highest-altitude.java
+++ LeetCode/Easy/find-the-highest-altitude.java
@@ -3,19 +3,4 @@
 // Difficulty: Easy
 // Language: java
 
-class Solution {
-    public int largestAltitude(int[] gain) {
-        int n = gain.length;
-        int[] ini = new int[n + 1];
-        int a = 0;
-
-        ini[0] = 0;
-        for (int i = 0; i < n; i++) {
-            a += gain[i];
-            ini[i + 1] = a;
-        }
-
-        Arrays.sort(ini);
-        return ini[n];
-    }
-}
+// Add your solution here!

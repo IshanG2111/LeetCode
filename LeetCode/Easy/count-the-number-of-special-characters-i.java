d9461ed350e562a2c07b6677f09b2deacbb14be8
Ishan <ishan.ghosh2004@gmail.com>
2026-06-21 00:11:42 +0530

    Added sync for Count the Number of Special Characters I

---
+++ LeetCode/Easy/count-the-number-of-special-characters-i.java
@@ -0,0 +1,6 @@
+// Title: Count the Number of Special Characters I
+// URL: https://leetcode.com/problems/count-the-number-of-special-characters-i/
+// Difficulty: Easy
+// Language: java
+
+// Add your solution here!

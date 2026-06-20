439476fc3c35bad71f76725311894ff9437c5a19
Ishan <ishan.ghosh2004@gmail.com>
2026-06-21 00:11:44 +0530

    Added sync for Angle Between Hands of a Clock

---
+++ LeetCode/Medium/angle-between-hands-of-a-clock.java
@@ -0,0 +1,6 @@
+// Title: Angle Between Hands of a Clock
+// URL: https://leetcode.com/problems/angle-between-hands-of-a-clock/
+// Difficulty: Medium
+// Language: java
+
+// Add your solution here!
